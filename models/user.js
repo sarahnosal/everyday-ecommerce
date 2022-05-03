74940ebc2a6b3e6485953e9b0cf6f9431dc24ed4
Sarah Nosal <nosalsm@gmail.com>
2022-05-03 16:59:35 -0500

    Completed user endpoints

--- models/user.js
+++ models/user.js
@@ -1,6 +1,94 @@
+const { database } = require('pg/lib/defaults');
 const db = require('../database');
 const pgp = require('pg-promise')({ capSQL: true });
 
 module.exports = class UserModel {
-    
+
+    /**
+   * Creates a new user record
+   * @param  {Object}      data [User data]
+   * @return {Object|null}      [Created user record]
+   */
+  async create(data) {
+      try {
+          const statement = pgp.helpers.insert(data, null, 'users') + 'RETURNING *';
+
+          const result = await database.query(statement);
+
+          if (result.rows?.length) {
+              return result.rows[0];
+          }
+
+          return null;
+      } catch(err) {
+          throw new Error(err);
+      }
+  }
+    /**
+   * Updates a user record
+   * @param  {Object}      data [User data]
+   * @return {Object|null}      [Updated user record]
+   */
+  async update(data) {
+      try {
+          const { id, ...params } = data;
+
+          const condition = pgp.as.format('WHERE id = ${id} RETURNING *', { id });
+          const statement = pgp.helpers.update(params, null, 'users') + condition;
+
+          const result = await database.query(statement);
+
+          if (result.rows?.length) {
+              return result.rows[0];
+          }
+          return null;
+      } catch(err) {
+          throw new Error(err);
+      }
+  }
+  /**
+   * Finds a user record by email
+   * @param  {String}      email [Email address]
+   * @return {Object|null}       [User record]
+   */
+  async findOneByEmail(email) {
+      try {
+
+        const statement = `SELECT * 
+                            FROM users
+                            WHERE email = $1`;
+        const values = [email];
+
+        const result = await database.query(statement, values);
+
+        if (result.rows?.length) {
+            return rows[0]
+        }
+        return null
+      } catch(err) {
+          throw new Error(err)
+      }
+  }
+  /**
+   * Finds a user record by ID
+   * @param  {String}      id [User ID]
+   * @return {Object|null}    [User Record]
+   */
+  async findOneById(id) {
+      try {
+        const statement = `SELECT *
+                            FROM users
+                            WHERE id = $1`;
+        const values = [id];
+
+        const result = await database.query(statement, values);
+
+        if (result.rows?.length) {
+            return result.rows[0]
+        }
+        return null
+      } catch(err) {
+          throw new Error(err);
+      }
+  }
 }
---
+++ services/UserService.js
@@ -0,0 +1,29 @@
+const createError = require('http-errors');
+const UserModel = require('../models/user');
+const UserModelInstance = new UserModel();
+
+module.exports = class UserService {
+    async get(data) {
+        const { id } = data;
+
+        try {
+            const user = await UserModelInstance.findOneById(id);
+
+            if (!user) {
+                throw createError(404, 'User record not found');
+            }
+            return user;
+        } catch(err) {
+            throw err;
+        }
+    }
+
+    async update(data) {
+        try{
+            const user = await UserModelInstance.update(data);
+            return user;
+        } catch(err) {
+            throw err
+        }
+    };
+}
