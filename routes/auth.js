335ef10bc118b0e9d730a506b1178bf94990772f
Sarah Nosal <nosalsm@gmail.com>
2022-05-02 22:13:55 -0500

    started authorization route

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const AuthService = require('../services/AuthService');
+const AuthServiceInstance = new AuthService();
 
+module.exports = (app, passport) => {
+    
+}
