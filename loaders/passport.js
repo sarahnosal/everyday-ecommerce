acd04c4673ef8d87cbae614a5c161a4c9d983ef9
Sarah Nosal <nosalsm@gmail.com>
2022-05-03 15:58:34 -0500

    passport initialized for login

--- loaders/passport.js
+++ loaders/passport.js
@@ -1,2 +1,31 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const AuthService = require('../services/AuthService');
+const AuthServiceInstance = new AuthService();
+
+module.exports = (app) => {
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        done(null, { id });
+    });
+
+    passport.use(new LocalStrategy(
+        async (username, password, done) => {
+            try {
+                const user = await AuthServiceInstance.login({ email: username, password });
+                return done(null, user);
+            } catch(err) {
+                return done(err);
+            }
+        }
+    ));
+
+    return passport;
+}
