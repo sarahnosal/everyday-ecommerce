9bcd7d905e534b96a4f0810a814609a5c78fb991
Sarah Nosal <nosalsm@gmail.com>
2022-05-03 15:50:09 -0500

    registration endpoint done

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,16 @@ const AuthService = require('../services/AuthService');
 const AuthServiceInstance = new AuthService();
 
 module.exports = (app, passport) => {
-    
+    app.use('/auth', router);
+
+    router.post('/register', async (req, res, next) => {
+        try {
+            const data = req.body;
+
+            const response = await AuthServiceInstance.register(data)
+            res.status(200).send(response)
+        } catch(err) {
+            next(err)
+        }
+    });
 }
