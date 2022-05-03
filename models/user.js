5a32ec37079ff7796a7a9b96ab7ad45fb0e3e124
Sarah Nosal <nosalsm@gmail.com>
2022-05-02 20:03:45 -0500

    Completed AuthService

---
+++ models/user.js
@@ -0,0 +1,6 @@
+const db = require('../database');
+const pgp = require('pg-promise')({ capSQL: true });
+
+module.exports = class UserModel {
+    
+}
--- services/AuthService.js
+++ services/AuthService.js
@@ -1,3 +1,42 @@
 const createError = require('http-errors');
 const UserModel = require('../models/user');
 const UserModelInstance = new UserModel();
+
+module.exports = class AuthService {
+
+    async register(data) {
+        const { email } = data;
+
+        try {
+
+            const user = await UserModelInstance.findOneByEmail(email);
+
+            if (user) {
+                throw createError(409, 'Email already in use');
+            }
+
+            return await UserModelInstance.create(data)
+        } catch(err) {
+            throw createError(500, err);
+        }
+    };
+
+    async login(data) {
+        const { email, password } = data;
+
+        try {
+            const user = await UserModelInstance.findOneByEmail(email);
+            if(!user) {
+                throw createError(401, 'Incorrect usermame or password');
+            }
+
+            if (user.password !== password) {
+                throw createError(401, 'Incorrect username or password');
+            }
+
+            return user;
+        } catch(err) {
+            throw createError(500, err);
+        }
+    }
+}
