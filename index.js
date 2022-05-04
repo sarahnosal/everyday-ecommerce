efbc2e8af17bd4e671f943c35f6a566053f9511a
Sarah Nosal <nosalsm@gmail.com>
2022-05-04 15:27:54 -0500

    Fixing config issues

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const loaders = require('./loaders')
+const express = require('express');
+const app = express();
+
+const loaders = require('./loaders');
+
+const { PORT } = require('./config');
 
 async function startServer() {
 
-  // Initialiez application
-  loaders(app)
+  // Init application loaders
+  loaders(app);
 
   // Start server
-  app.listen(port, () => {
-    console.log(`Server listening on PORT ${port}`)
+  app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
   })
 }
 
--- services/ProductService.js
+++ services/ProductService.js
@@ -3,26 +3,38 @@ const ProductModel = require('../models/product');
 const ProductModelInstance = new ProductModel();
 
 module.exports = class ProductService {
-    async list(options) {
-        try {
 
-            const products = await ProductModelInstance.find(options);
-            return products
-        } catch(err) {
-            throw err
-        }
-    };
+  async list(options) {
 
-    async get(id) {
-        try {
-            const product = await ProductModelInstance.findOne(id);
+    try {
+      // Load products
+      const products = await ProductModelInstance.find(options);
 
-            if (!product) {
-                throw createError(404, 'Product not found');
-            }
-            return product;
-        } catch(err) {
-            throw err;
-        }
-    };
+      return products;
+
+    } catch(err) {
+      throw err;
+    }
+
+  };
+
+  async get(id) {
+
+    try {
+      // Check if product exists
+      const product = await ProductModelInstance.findOne(id);
+
+      // If no product found, reject
+      if (!product) {
+        throw createError(404, 'Product not found');
+      }
+
+      return product;
+
+    } catch(err) {
+      throw err;
+    }
+
+  };
+
 }
