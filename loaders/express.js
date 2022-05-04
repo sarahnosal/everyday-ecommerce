9b12e47bbc686170cedcafe4c7063f19307c09c1
Sarah Nosal <nosalsm@gmail.com>
2022-05-04 14:44:01 -0500

    Added loaders

---
+++ loaders/express.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const session = require('express-session');
+const { SESSION_SECRET } = require('../config');
+
+module.exports = (app) => {
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true}));
+    app.set('trust proxy', 1);
+    app.use(
+        session({
+            secret: SESSION_SECRET,
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                secure: false,
+                maxAge: 24 * 60 * 60 * 1000
+            }
+        })
+    );
+    return app;
+}
