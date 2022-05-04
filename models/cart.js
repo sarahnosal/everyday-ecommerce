f0fdfeb58a9ee60ed6bc64879e6db1c6c38eba5d
Sarah Nosal <nosalsm@gmail.com>
2022-05-04 15:37:40 -0500

    Added comments in models

--- models/cart.js
+++ models/cart.js
@@ -3,73 +3,95 @@ const moment = require('moment');
 const pgp = require('pg-promise')({ capSQL: true });
 
 module.exports = class CartModel {
-    constructor(data = {}) {
-        this.created = data.created || moment.utc().toISOString();
-        this.modified = moment.utc().toISOString();
-        this.converted = data.converted || null;
-        this.isActive = data.isActive || true;
-    }
-   /**
+
+  constructor(data = {}) {
+    this.created = data.created || moment.utc().toISOString();
+    this.modified = moment.utc().toISOString();
+    this.converted = data.converted || null;
+    this.isActive = data.isActive || true;
+  }
+
+  /**
    * Creates a new cart for a user
    * @param  {Object}      data [User data]
    * @return {Object|null}      [Created user record]
    */
   async create(userId) {
-      try {
-          const data = { userId, ...this}
+    try {
 
-          const statement = pgp.helpers.insert(data, null, 'carts') + 'RETURNING *';
+      const data = { userId, ...this}
 
-          const result = await db.query(statement);
+      // Generate SQL statement - using helper for dynamic parameter injection
+      const statement = pgp.helpers.insert(data, null, 'carts') + 'RETURNING *';
+  
+      // Execute SQL statment
+      const result = await db.query(statement);
 
-          if (result.rows?.length) {
-              return result.rows[0];
-          }
-          return null;
-      } catch(err) {
-          throw new Error(err);
+      if (result.rows?.length) {
+        return result.rows[0];
       }
+
+      return null;
+
+    } catch(err) {
+      throw new Error(err);
+    }
   }
-   /**
+
+  /**
    * Loads a cart by User ID
    * @param  {number}      id [User ID]
    * @return {Object|null}    [Cart record]
    */
   static async findOneByUser(userId) {
-      try {
-          const statement = `SELECT *
-                             FROM carts
-                             WHERE 'userId' = $1`;
-          const values = [userId];
-          const result = await db.query(statement, values);
+    try {
 
-          if (result.rows?.length) {
-              return result.rows[0];
-          }
-          return null;
-      } catch(err) {
-          throw new Error(err);
+      // Generate SQL statement
+      const statement = `SELECT *
+                         FROM carts
+                         WHERE "userId" = $1`;
+      const values = [userId];
+  
+      // Execute SQL statment
+      const result = await db.query(statement, values);
+
+      if (result.rows?.length) {
+        return result.rows[0];
       }
+
+      return null;
+
+    } catch(err) {
+      throw new Error(err);
+    }
   }
-   /**
+
+  /**
    * Loads a cart by ID
    * @param  {number}      id [Cart ID]
    * @return {Object|null}    [Cart record]
    */
   static async findOneById(id) {
-      try {
-          const statement = `SELECT *
-                             FROM carts
-                             WHERE "id" = $1`;
-          const values = [id];
-          const result = await db.query(statement, values);
+    try {
 
-          if (result.rows?.length) {
-              return result.rows[0];
-          }
-          return null;
-      } catch(err) {
-          throw new Error(err);
+      // Generate SQL statement
+      const statement = `SELECT *
+                         FROM carts
+                         WHERE id" = $1`;
+      const values = [id];
+  
+      // Execute SQL statment
+      const result = await db.query(statement, values);
+
+      if (result.rows?.length) {
+        return result.rows[0];
       }
+
+      return null;
+
+    } catch(err) {
+      throw new Error(err);
+    }
   }
+
 }
--- models/user.js
+++ models/user.js
@@ -1,94 +1,114 @@
-const { database } = require('pg/lib/defaults');
 const db = require('../db');
 const pgp = require('pg-promise')({ capSQL: true });
 
 module.exports = class UserModel {
 
-    /**
+  /**
    * Creates a new user record
    * @param  {Object}      data [User data]
    * @return {Object|null}      [Created user record]
    */
   async create(data) {
-      try {
-          const statement = pgp.helpers.insert(data, null, 'users') + 'RETURNING *';
+    try {
 
-          const result = await db.query(statement);
+      // Generate SQL statement - using helper for dynamic parameter injection
+      const statement = pgp.helpers.insert(data, null, 'users') + 'RETURNING *';
+  
+      // Execute SQL statment
+      const result = await db.query(statement);
 
-          if (result.rows?.length) {
-              return result.rows[0];
-          }
-
-          return null;
-      } catch(err) {
-          throw new Error(err);
+      if (result.rows?.length) {
+        return result.rows[0];
       }
+
+      return null;
+
+    } catch(err) {
+      throw new Error(err);
+    }
   }
-    /**
+
+  /**
    * Updates a user record
    * @param  {Object}      data [User data]
    * @return {Object|null}      [Updated user record]
    */
   async update(data) {
-      try {
-          const { id, ...params } = data;
+    try {
 
-          const condition = pgp.as.format('WHERE id = ${id} RETURNING *', { id });
-          const statement = pgp.helpers.update(params, null, 'users') + condition;
+      const { id, ...params } = data;
 
-          const result = await db.query(statement);
+      // Generate SQL statement - using helper for dynamic parameter injection
+      const condition = pgp.as.format('WHERE id = ${id} RETURNING *', { id });
+      const statement = pgp.helpers.update(params, null, 'users') + condition;
+  
+      // Execute SQL statment
+      const result = await db.query(statement);
 
-          if (result.rows?.length) {
-              return result.rows[0];
-          }
-          return null;
-      } catch(err) {
-          throw new Error(err);
+      if (result.rows?.length) {
+        return result.rows[0];
       }
+
+      return null;
+
+    } catch(err) {
+      throw new Error(err);
+    }
   }
+
   /**
    * Finds a user record by email
    * @param  {String}      email [Email address]
    * @return {Object|null}       [User record]
    */
   async findOneByEmail(email) {
-      try {
+    try {
 
-        const statement = `SELECT * 
-                            FROM users
-                            WHERE email = $1`;
-        const values = [email];
+      // Generate SQL statement
+      const statement = `SELECT *
+                         FROM users
+                         WHERE email = $1`;
+      const values = [email];
+  
+      // Execute SQL statment
+      const result = await db.query(statement, values);
 
-        const result = await db.query(statement, values);
-
-        if (result.rows?.length) {
-            return rows[0]
-        }
-        return null
-      } catch(err) {
-          throw new Error(err)
+      if (result.rows?.length) {
+        return result.rows[0]
       }
+  
+      return null;
+
+    } catch(err) {
+      throw new Error(err);
+    }
   }
+
   /**
    * Finds a user record by ID
    * @param  {String}      id [User ID]
    * @return {Object|null}    [User Record]
    */
   async findOneById(id) {
-      try {
-        const statement = `SELECT *
-                            FROM users
-                            WHERE id = $1`;
-        const values = [id];
+    try {
 
-        const result = await db.query(statement, values);
+      // Generate SQL statement
+      const statement = `SELECT *
+                         FROM users
+                         WHERE id = $1`;
+      const values = [id];
+  
+      // Execute SQL statment
+      const result = await db.query(statement, values);
 
-        if (result.rows?.length) {
-            return result.rows[0]
-        }
-        return null
-      } catch(err) {
-          throw new Error(err);
+      if (result.rows?.length) {
+        return result.rows[0]
       }
+  
+      return null;
+
+    } catch(err) {
+      throw new Error(err);
+    }
   }
 }
