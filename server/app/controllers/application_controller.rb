2cfc45a7425f73e3b37e1cb2e2631a897d51c360
Sarah Nosal <nosalsm@gmail.com>
2022-06-21 18:00:23 -0500

    Adding methods to application controller

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    def new
+        initialize_resource
+    end
+ 
+    def create
+       build_resource
+ 
+       resource.save!
+    end
+ 
+    def update
+        resource.update! resource_params
+    end
+ 
+    def destroy
+        resource.destroy!
+    end
 end
