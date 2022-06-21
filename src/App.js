f5e39f165b9997908490ea0de4e54c9ff49e5ed0
Sarah Nosal <nosalsm@gmail.com>
2022-06-21 17:14:13 -0500

    updating file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+import './App.css';
+import HomePage from './HomePage';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+
+function HomePage() {
+
+
+
+    return (
+        <div>
+            <img className= 'homepageImage' src='./images/152530940-huge.jpg' alt='HomePage image'/>
+        </div>
+    )
+}
+
+export default HomePage
