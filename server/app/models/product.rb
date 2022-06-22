2f6ee56fb9da377078b80522bfba808a30ee8ed9
Sarah Nosal <nosalsm@gmail.com>
2022-06-21 19:17:47 -0500

    Added search capabilities

--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+    class << self
+        def search_by params = {}
+          params = params.try(:symbolize_keys) || {}
+
+          collection = all
+
+          if params[:term].present?
+            collection = collection.where('name ILIKE ?', "#{ params[:term] }%")
+          end
+    
+          collection
+        end
+    end
 end
--- server/spec/models/product_spec.rb
+++ server/spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.search_by' do
+    let(:relation) { double }
+
+    before { expect(Product).to receive(:all).and_return(relation) }
+
+    context do
+      it { expect { Product.search_by }.to_not raise_error }
+    end
+
+    context do
+      before { expect(relation).to receive(:where).with('name ILIKE ?', 'abc%') }
+
+      it { expect { Product.search_by 'term' => 'abc' }.to_not raise_error }
+    end
+  end
 end
